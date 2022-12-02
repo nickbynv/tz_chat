f4f51df26cff9852a1a735c767655f778fb1b8d5
Nick <99430009+ntg00d@users.noreply.github.com>
2022-12-02 20:01:42 +0300

    commit

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -3,24 +3,24 @@ import { memo, useState } from 'react'
 export default memo((props: {
     setUser: React.Dispatch<React.SetStateAction<string | null>>
 }) => {
-    const [value, setValue] = useState('')
+    const [username, setUsername] = useState('')
 
     return (
         <form className='flex flex-col' onSubmit={(e) => {
             e.preventDefault()
 
-            if (value.length) {
-                sessionStorage.setItem('user', value)
+            if (username.length) {
+                sessionStorage.setItem('user', username)
                 props.setUser(sessionStorage.getItem('user'))
             }
 
-            setValue('')
+            setUsername('')
         }}>
             <input
                 className='ring-[.5px] ring-[#a7a7a7] w-60 h-10 px-2 rounded-lg mb-3'
                 placeholder='Enter your name'
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
             />
             <button
                 className='bg-blue-600 py-2 rounded-lg text-white hover:bg-[#001aac] transition-colors'
